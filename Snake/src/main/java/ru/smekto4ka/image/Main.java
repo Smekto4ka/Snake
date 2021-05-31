046942702691471943f4091b51acd906d1e19f44
Smekto4ka <barsalekcandr@mail.ru>
2021-05-31 19:30:45 +0300

    add sort image

--- Snake/src/main/java/ru/smekto4ka/image/Main.java
+++ Snake/src/main/java/ru/smekto4ka/image/Main.java
@@ -3,59 +3,53 @@ package ru.smekto4ka.image;
 import ru.smekto4ka.image.service.ImageService;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-
-import java.io.*;
-import org.apache.batik.transcoder.image.PNGTranscoder;
-import org.apache.batik.transcoder.TranscoderInput;
-import org.apache.batik.transcoder.TranscoderOutput;
-import java.nio.file.Paths;
-import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
         new Main().css();
 
     }
-// Main.class.getResource("/test.jpg").getPath()
+
+    // Main.class.getResource("/test.jpg").getPath()
     public void css() {
         try {
 
+            List<BufferedImage> list = new ArrayList<>();
             BufferedImage im = ImageIO.read(new File("C:\\Games\\GitHub\\Snake\\Snake\\src\\main\\resources\\pizza.jpg"));
-            List<BufferedImage> images = new ArrayList<>();
-            for (int i = 0; i < 10; i++) {
-                images.add(im);
+            list.add(im);
+            int height = im.getHeight();
+            int width = im.getWidth();
+            int length = 30;
+            int vich_h = height / (length + 10);
+            int vich_w = width / (length + 10);
+
+            for (int i = 0; i < length; i++) {
+                BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+                Graphics g = image.getGraphics();
+                g.drawImage(ImageIO.read(new File("C:\\Games\\GitHub\\Snake\\Snake\\src\\main\\resources\\pizza.jpg"))
+                        , 0, 0, width, height, null);
+                list.add(image);
+
+                height -= vich_h;
+                width -= vich_w;
             }
-            BufferedImage bf = new ImageService().css(images);
-            File file =  new File("C:\\Games\\GitHub\\Snake\\Snake\\src\\main\\resources\\test.jpg");
-            ImageIO.write(bf, "JPG",file);
+
+
+            BufferedImage bf = new ImageService().css(list);
+            File file = new File("C:\\Games\\GitHub\\Snake\\Snake\\src\\main\\resources\\test.jpg");
+            ImageIO.write(bf, "JPG", file);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
-    public void svg(){
-        //Step -1: We read the input SVG document into Transcoder Input
-        //We use Java NIO for this purpose
-        String svg_URI_input = Paths.get("chessboard.svg").toUri().toURL().toString();
-        TranscoderInput input_svg_image = new TranscoderInput(svg_URI_input);
-        //Step-2: Define OutputStream to PNG Image and attach to TranscoderOutput
-        OutputStream png_ostream = new FileOutputStream("chessboard.png");
-        TranscoderOutput output_png_image = new TranscoderOutput(png_ostream);
-        // Step-3: Create PNGTranscoder and define hints if required
-        PNGTranscoder my_converter = new PNGTranscoder();
-        // Step-4: Convert and Write output
-        my_converter.transcode(input_svg_image, output_png_image);
-        // Step 5- close / flush Output Stream
-        png_ostream.flush();
-        png_ostream.close();
-    }
+
 }
--- Snake/src/main/java/ru/smekto4ka/image/service/ImageService.java
+++ Snake/src/main/java/ru/smekto4ka/image/service/ImageService.java
@@ -1,22 +1,68 @@
 package ru.smekto4ka.image.service;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.util.*;
 import java.util.List;
 
 public class ImageService {
 
     public BufferedImage css(List<BufferedImage> imgs) {
-        int width = imgs.get(0).getWidth();
-        int height = imgs.get(0).getHeight();
+        imgs.sort(new SortWidthImage());
+        int widthMax = imgs.get(0).getWidth() * 3;
 
-        BufferedImage im = new BufferedImage(imgs.size() * width, height, BufferedImage.TYPE_BYTE_INDEXED);
-        int i = 0;
+        imgs.sort(new SortHeightImage());
+        List<Integer> heightLevel = new ArrayList<>();
+        heightLevel.add(0);
+        List<Integer> balanceWidth = new ArrayList<>();
+
+        int maxLevel = 0;
+        Map<BufferedImage, List<Integer>> coordinates = new HashMap<>();
+        BufferedImage godImage = new BufferedImage(widthMax, imgs.get(0).getHeight(), BufferedImage.TYPE_INT_RGB);
+        boolean flag = true;
+        newImage:
         for (BufferedImage image : imgs) {
-            im.getGraphics().drawImage(image, i, 0, null);
-            i += width;
-        }
+            if (flag) {
+                godImage.getGraphics().drawImage(image, 0, 0,null);
+                // coordinates.put(image , new ArrayList<>());
+                heightLevel.add(image.getHeight());
+                balanceWidth.add( image.getWidth());
+                flag = false;
+                continue;
+            }
+            for (int i = 0; i <= maxLevel; i++) {
+                int widthLevel = balanceWidth.get(i);
+                if ((widthMax - widthLevel) >= image.getWidth()) {
+                    godImage.getGraphics().drawImage(image, widthLevel, heightLevel.get(i), null);
+                    balanceWidth.set(i, widthLevel + image.getWidth());
+                    continue newImage;
+                }
+            }
 
-return im;
+            BufferedImage im = new BufferedImage(widthMax, godImage.getHeight() + image.getHeight(), BufferedImage.TYPE_INT_RGB);
+            im.getGraphics().drawImage(godImage, 0, 0, null);
+            heightLevel.add(im.getHeight());
+            im.getGraphics().drawImage(image, 0, godImage.getHeight(), null);
+            balanceWidth.add(image.getWidth());
+            godImage = im;
+            maxLevel++;
+        }
+        return godImage;
     }
 
+}
+
+class SortHeightImage implements Comparator<BufferedImage> {
+    @Override
+    public int compare(BufferedImage o1, BufferedImage o2) {
+        return o2.getHeight() - o1.getHeight();
+    }
+}
+
+class SortWidthImage implements Comparator<BufferedImage> {
+    @Override
+    public int compare(BufferedImage o1, BufferedImage o2) {
+        return o2.getWidth() - o1.getWidth();
+    }
 }
